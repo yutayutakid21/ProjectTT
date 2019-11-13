863c4d39403db7d2e6a0ee7930d1d9f42a04eb5f
Yuta Fujii <yutayutakid21@gmail.com>
2019-11-13 18:47:36 +0900

    Initial Commit

---
+++ ProjectTT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectTT
+//
+//  Created by Fujii Yuta on 2019/11/13.
+//  Copyright © 2019 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
